45c4936771f3f4cec831fdb93dedd4e31adf055d
Suml <2906247040@qq.com>
2026-07-03 18:16:29 +0800

    修复发现页全部加入书架不生效的bug

--- app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/explore/ExploreShowActivity.kt
@@ -205,8 +205,9 @@ class ExploreShowActivity : VMBaseActivity<ActivityExploreShowBinding, ExploreSh
 
     override fun upGroup(requestCode: Int, groupId: Long) {
         if (requestCode == REQUEST_CODE_ADD_ALL_TO_SHELF) {
-            toastOnUi(getString(R.string.adding_books, currentExploreFragment()?.getBooksCount() ?: 0))
-            viewModel.addAllToShelf(groupId)
+            val books = currentExploreFragment()?.getSearchBooks() ?: emptyList()
+            toastOnUi(getString(R.string.adding_books, books.size))
+            viewModel.addAllToShelf(groupId, books)
         }
     }
 
